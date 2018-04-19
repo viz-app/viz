7c53e889ac3d481d8a7b33ca4f1e9651ac16d741
Fabien Allanic <fabien.allanic@gmail.com>
2018-04-19 15:53:35 -0500

    Linting

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="App-title">Welcome to React</h1>
-			</header>
-			<p className="App-intro">
-				To get started, edit <code>src/App.jsx</code> and save to reload.
-			</p>
-		</div>
-	);
+	<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<h1 className="App-title">Welcome to React</h1>
+		</header>
+		<p className="App-intro">
+			To get started, edit <code>src/App.jsx</code> and save to reload.
+		</p>
+	</div>
+);
 
 export default App;
