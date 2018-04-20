4028660c78d5972c5c59484abacb0e8fc7055254
Maxime Burlat <maximeburlat@gmail.com>
2018-04-20 16:25:37 -0500

    Revert "Parsing uri when the user selects a file or folder"

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,8 @@ class App extends React.Component {
 		super();
 		ipcRenderer.on('fileSelectedByUser', (event, arg) => {
 			// prints whatever file has been selected by the user
-			console.log(`file selected by user ${JSON.stringify(arg)}`);
+			console.log(`file selected by user ${arg}`);
 			// TODO actually set the state / context, load the image,etc.
-		});
-		ipcRenderer.on('leftKeyPressed', (event, arg) => {
-			console.log(`Left key pressed ${arg}`);
-			// TODO actually change the state
-		});
-		ipcRenderer.on('rightKeyPressed', (event, arg) => {
-			console.log(`Right key pressed ${arg}`);
-			// TODO actually change the state
 		});
 	}
 
