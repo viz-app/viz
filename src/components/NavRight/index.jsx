599abb5d72e0c47a31509e3245500fd15b34f1b4
Maxime Burlat <maximeburlat@gmail.com>
2018-04-20 13:25:33 -0500

    icons added in side bars

--- src/components/NavRight/index.jsx
+++ src/components/NavRight/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import FontIcon from 'react-toolbox/lib/font_icon';
+
 import './NavRight.css';
 
-const NavRight = () => <div className="navRight">This is the NavRight component</div>;
+const NavRight = () => (
+	<div className="navRight">
+		<span>
+			<FontIcon className="icon" value="delete" />
+			<FontIcon className="icon" value="share" />
+			<FontIcon className="icon" value="rotate_left" />
+			<FontIcon className="icon" value="rotate_right" />
+		</span>
+	</div>
+);
 
 export default NavRight;
