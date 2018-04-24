d080a62be42cbdb42ba4485f15718862aca6e677
Maxime Burlat <maximeburlat@gmail.com>
2018-04-24 08:26:31 -0500

    added arrows, just need to center them and add the hooks

--- src/components/PictureViewer/index.jsx
+++ src/components/PictureViewer/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import FontIcon from 'react-toolbox/lib/font_icon';
 import './PictureViewer.css';
 import FileInfoContext from './../../FileInfoContext';
 
 const PictureViewer = () => (
 	<div className="pictureViewer">
+		<div className="arrow left-arrow">
+			<span>
+				<FontIcon className="icon" value="keyboard_arrow_left" />
+			</span>
+		</div>
 		{/* creating a consumer for the context to display the current image */}
 		<FileInfoContext.Consumer>
 			{({ folder, currentFileIndex, filesInFolder }) => (
 				<img alt="" src={`file://${folder}/${filesInFolder[currentFileIndex]}`} />
 			)}
 		</FileInfoContext.Consumer>
+		<div className="arrow right-arrow">
+			<span>
+				<FontIcon className="icon" value="keyboard_arrow_right" />
+			</span>
+		</div>
 	</div>
 );
 
