c145e95850708e121e32760248e295237bb70f17
Maxime Burlat <maximeburlat@gmail.com>
2018-04-29 17:51:44 -0500

    got a basic slider, no handlers attached

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import './Slider.css';
+import FileInfoContext from '../../FileInfoContext';
 
-const Slider = () => <div className="slider">This is the Slider component with images</div>;
+const Slider = () => (
+	<div className="slider">
+		<FileInfoContext.Consumer>
+			{({ folder, filesInFolder }) => {
+				const imagesLinks = filesInFolder.map(imageAddress => (
+					<li className="slider-item">
+						<img alt="some picz" src={`file://${folder}/${imageAddress}`} />
+					</li>
+				));
+				return <ul className="slider-list">{imagesLinks}</ul>;
+			}}
+		</FileInfoContext.Consumer>
+	</div>
+);
+
+// const Slider = () => (
+// <div className="slider">
+// <FileInfoContext.Consumer>
+//   {({ folder, currentFileIndex, filesInFolder })} => {
+//     for (imageName of filesInFolder){
+
+//     }
+//   }
+// </FileInfoContext.Consumer>
+// </div>);
 
 export default Slider;
