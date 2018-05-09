2462ba65d2b6a9c26d3b10df69f59c1ba618dd63
Maxime Burlat <maximeburlat@gmail.com>
2018-05-09 18:07:06 -0500

    first version of rotate 90 and 270 working

--- src/components/PictureViewer/index.jsx
+++ src/components/PictureViewer/index.jsx
@@ -5,34 +5,86 @@ import FontIcon from 'react-toolbox/lib/font_icon';
 import './PictureViewer.css';
 import FileInfoContext from './../../FileInfoContext';
 
-const PictureViewer = () => (
-	<div className="pictureViewer">
-		{/* creating a consumer for the context to display the current image */}
-		<FileInfoContext.Consumer>
-			{({
-				folder,
-				currentFileIndex,
-				currentFileRotation,
-				filesInFolder,
-				onLeftArrow,
-				onRightArrow
-			}) => (
-				<div className="bufferDiv">
-					<div className="arrow left-arrow" onClick={onLeftArrow} role="button" tabIndex="0">
-						<FontIcon className="icon" value="keyboard_arrow_left" />
-					</div>
-					<img
-						className={`rotate${currentFileRotation}`}
-						alt={`some picz ${currentFileIndex}`}
-						src={`file://${folder}/${filesInFolder[currentFileIndex]}`}
-					/>
-					<div className="arrow right-arrow" onClick={onRightArrow} role="button" tabIndex="0">
-						<FontIcon className="icon" value="keyboard_arrow_right" />
-					</div>
-				</div>
-			)}
-		</FileInfoContext.Consumer>
-	</div>
-);
+class PictureViewer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.itemRef = React.createRef();
+	}
+
+	render() {
+		return (
+			<div className="pictureViewer">
+				{/* creating a consumer for the context to display the current image */}
+				<FileInfoContext.Consumer>
+					{({
+						folder,
+						currentFileIndex,
+						currentFileRotation,
+						filesInFolder,
+						onLeftArrow,
+						onRightArrow
+					}) => (
+						<div className="bufferDiv" ref={this.itemRef}>
+							<div className="arrow left-arrow" onClick={onLeftArrow} role="button" tabIndex="0">
+								<FontIcon className="icon" value="keyboard_arrow_left" />
+							</div>
+							<img
+								className={`rotate${currentFileRotation}`}
+								style={{
+									transform: `scale(${
+										currentFileRotation === 90 || currentFileRotation === 270
+											? this.itemRef.current.getBoundingClientRect().height /
+											  this.itemRef.current.getBoundingClientRect().width
+											: 1
+									} ) rotate(${currentFileRotation}deg)`
+								}}
+								alt={`some picz ${currentFileIndex}`}
+								src={`file://${folder}/${filesInFolder[currentFileIndex]}`}
+							/>
+							<div className="arrow right-arrow" onClick={onRightArrow} role="button" tabIndex="0">
+								<FontIcon className="icon" value="keyboard_arrow_right" />
+							</div>
+						</div>
+					)}
+				</FileInfoContext.Consumer>
+			</div>
+		);
+	}
+}
+
+// const PictureViewer = () => (
+// 	<div className="pictureViewer">
+// 		{/* creating a consumer for the context to display the current image */}
+// 		<FileInfoContext.Consumer>
+// 			{({
+// 				folder,
+// 				currentFileIndex,
+// 				currentFileRotation,
+// 				filesInFolder,
+// 				onLeftArrow,
+// 				onRightArrow
+// 			}) => (
+// 				<div className="bufferDiv">
+// 					<div className="arrow left-arrow" onClick={onLeftArrow} role="button" tabIndex="0">
+// 						<FontIcon className="icon" value="keyboard_arrow_left" />
+// 					</div>
+// 					<img
+// 						className={`rotate${currentFileRotation}`}
+// 						style={{
+// 							transform: `scale(${
+// 								currentFileRotation === 90 || currentFileRotation === 270 ? 0.5 : 1
+// 							} ) rotate(${currentFileRotation}deg)`
+// 						}}
+// 						alt={`some picz ${currentFileIndex}`}
+// 						src={`file://${folder}/${filesInFolder[currentFileIndex]}`}
+// 					/>
+// 					<div className="arrow right-arrow" onClick={onRightArrow} role="button" tabIndex="0">
+// 						<FontIcon className="icon" value="keyboard_arrow_right" />
+// 					</div>
+// 				</div>
+// 			)}
+// 		</FileInfoContext.Consumer>
+// 	</div>
+// );
 
 export default PictureViewer;
